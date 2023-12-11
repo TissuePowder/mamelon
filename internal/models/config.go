5f077b48d8e7749cdbeb37ca0119ad9187cd9df0
TissuePowder <nillyhan.xt@gmail.com>
2023-12-11 13:24:23 +0600

    Initial config and model

---
+++ internal/models/config.go
@@ -0,0 +1,30 @@
+package models
+
+type Bot struct {
+	Token    string   `json:"token"`
+	Channels []string `json:"channels"`
+	Trigger  string   `json:"trigger"`
+	Interval string   `json:"interval"`
+	Mentions []string `json:"mentions"`
+}
+
+type Scrape struct {
+	Instances []string `json:"instances"`
+	Interval  string   `json:"interval"`
+	Usernames []string `json:"usernames"`
+}
+
+type DeepL struct {
+	Url string `json:"url"`
+	Key string `json:"key"`
+}
+
+type Translate struct {
+	DeepL DeepL `json:"deepl"`
+}
+
+type Config struct {
+	Bot       Bot
+	Scrape    Scrape
+	Translate Translate
+}
